8cf6dc66009d0a52aa5cef194e357f3d3f7df710
SUMIT RAI <121968338+SUMIT74184@users.noreply.github.com>
2026-06-23 18:28:45 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findMin(int[] nums) {
+        Arrays.sort(nums);
+        int res =0;
+        for(int n:nums){
+            res = nums[0];
+        }
+        return res;
+    }
+}
