f20c7f87fccd4c2473dc72b2d0aa8301cb22a04c
SUMIT RAI <121968338+SUMIT74184@users.noreply.github.com>
2026-07-02 22:35:37 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> minHeap =
+                new PriorityQueue<>(Collections.reverseOrder());
+        for(int s : stones){
+            minHeap.offer(s);
+        }
+
+        while(minHeap.size() > 1){
+            int first = minHeap.poll();
+            int second = minHeap.poll();
+            if(second != first){
+                minHeap.offer(first - second);
+            }
+        }
+        // if nothing is present just return 0
+        minHeap.offer(0);
+        return Math.abs(minHeap.peek());
+    }
+}
